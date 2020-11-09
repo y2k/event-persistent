3dc3dbb67faa6e5b6ad95dab37acabf1a5b09de0
y2k <itwisterlx@gmail.com>
2020-11-09 15:05:51 +0300

    init commit

---
+++ lib/Library.fs
@@ -0,0 +1,48 @@
+﻿module EventPersistent.Persistent
+
+open LiteDB
+open System.Text.Json
+open System.Text.Json.Serialization
+
+let private options = JsonSerializerOptions()
+options.Converters.Add(JsonFSharpConverter())
+
+[<CLIMutable>]
+type Wrapper = { id: int; value: string }
+
+let private serialize x = JsonSerializer.Serialize(x, options)
+let private deserialize (s: string) = JsonSerializer.Deserialize(s, options)
+
+let create (db: LiteDatabase, name: string, init: 's, f: 's -> 'msg -> 's) =
+    let col = db.GetCollection<Wrapper>(name)
+    let index = ref 0
+    let state = ref init
+
+    fun (fxs: _ -> 'msg list) ->
+        async {
+            db.BeginTrans() |> ignore
+
+            let prevs =
+                col.Find(Query.GT("_id", BsonValue.op_Implicit !index))
+
+            for aw in prevs do
+                let a = aw.value
+                let x = deserialize a
+                index := aw.id
+                state := f !state x
+
+            let result = !state
+
+            let xs = fxs !state
+
+            for x in xs do
+                state := f !state x
+                let a = serialize x
+                let aw = { id = 0; value = a }
+                col.Insert aw |> ignore
+                index := aw.id
+
+            db.Commit() |> ignore
+
+            return result
+        }
---
+++ test/Tests.fs
@@ -0,0 +1,80 @@
+module Tests
+
+open System.IO
+open Xunit
+open Swensen.Unquote
+open EventPersistent
+
+module FrameworkExample =
+    type User = User of string
+    type Url = Url of string
+    type State = { items: Map<User, Url Set> }
+
+    type Msg =
+        | UrlAdded of user: User * url: Url
+        | UrlRemoved of user: User * url: Url
+
+    module Domain =
+        let reduce state =
+            function
+            | UrlAdded (user, url) ->
+                let urls =
+                    Map.tryFind user state.items
+                    |> Option.defaultValue Set.empty
+                    |> Set.add url
+
+                { state with
+                      items = Map.add user urls state.items }
+            | UrlRemoved (user, url) ->
+                let urls =
+                    Map.tryFind user state.items
+                    |> Option.defaultValue Set.empty
+                    |> Set.remove url
+
+                { state with
+                      items = Map.add user urls state.items }
+
+open FrameworkExample
+
+[<Fact>]
+let test () =
+    use db =
+        new LiteDB.LiteDatabase(new MemoryStream())
+
+    async {
+        let reducer =
+            Persistent.create (db, "log", { items = Map.empty }, Domain.reduce)
+
+        let! _ =
+            reducer (fun _ ->
+                [ UrlAdded(User "user1", Url "url1")
+                  UrlAdded(User "user2", Url "url2") ])
+
+        let! actual = reducer (fun _ -> [])
+
+        let expected =
+            { items =
+                  Map.ofList [ User "user1", Set.singleton (Url "url1")
+                               User "user2", Set.singleton (Url "url2") ] }
+
+        test <@ actual = expected @>
+
+        let! _ = reducer (fun _ -> [ UrlRemoved(User "user2", Url "url2") ])
+
+        let! actual = reducer (fun _ -> [])
+
+        let expected =
+            { items =
+                  Map.ofList [ User "user1", Set.singleton (Url "url1")
+                               User "user2", Set.empty ] }
+
+        test <@ actual = expected @>
+
+        let reducer =
+            Persistent.create (db, "log", { items = Map.empty }, Domain.reduce)
+
+        let! actual = reducer (fun _ -> [])
+
+        test <@ actual = expected @>
+    }
+    |> Async.RunSynchronously
