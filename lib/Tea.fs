6facca93e67e50be7df8099dcb0bae0bf49d1c22
y2k <itwisterlx@gmail.com>
2021-05-07 14:02:47 +0300

    refactoring

--- lib/Tea.fs
+++ lib/Tea.fs
@@ -10,11 +10,11 @@ type 'e t =
     private
         { mutex: SemaphoreSlim
           mailbox: MailboxProcessor<Cmd>
-          xs: ('e list -> unit) list ref }
+          updateFs: ('e list -> unit) list ref }
 
 let init () =
     { mutex = new SemaphoreSlim(1)
-      xs = ref []
+      updateFs = ref []
       mailbox =
           MailboxProcessor.Start
               (fun inbox ->
@@ -30,6 +30,25 @@ let init () =
                                   pendingEvents := []
                   }) }
 
+let private dispatch state t f =
+    async {
+        do! t.mutex.WaitAsync() |> Async.AwaitTask
+
+        let oldState = !state
+        let state', es = f oldState
+        state := state'
+
+        for u in !t.updateFs do
+            u es
+
+        t.mutex.Release() |> ignore
+
+        if not <| List.isEmpty es then
+            t.mailbox.Post Notify
+
+        return oldState
+    }
+
 let make (t: 'event t) (initState: 'state) (merge: 'state -> 'event -> 'state) =
     let state = ref initState
 
@@ -37,26 +56,9 @@ let make (t: 'event t) (initState: 'state) (merge: 'state -> 'event -> 'state) =
         for e in es do
             state := merge !state e
 
-    t.xs := update :: !t.xs
+    t.updateFs := update :: !t.updateFs
 
-    fun f ->
-        async {
-            do! t.mutex.WaitAsync() |> Async.AwaitTask
-
-            let oldState = !state
-            let (state', es) = f oldState
-            state := state'
-
-            for u in !t.xs do
-                u es
-
-            t.mutex.Release() |> ignore
-
-            if not <| List.isEmpty es then
-                t.mailbox.Post Notify
-
-            return oldState
-        }
+    dispatch state t
 
 let waitForChanges (t: 'event t) =
     t.mailbox.PostAndAsyncReply ListenForUpdate
